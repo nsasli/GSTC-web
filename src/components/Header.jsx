b035f4f6f211914a34116b63cd5eedac4f287053
Handi Priyono <handimacbook@gmail.com>
2024-06-30 23:54:31 +0700

    fix: adjust width

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -104,7 +104,7 @@ export function Header() {
                       d="m12 10.8l-3.9 3.9q-.275.275-.7.275t-.7-.275q-.275-.275-.275-.7t.275-.7l4.6-4.6q.3-.3.7-.3t.7.3l4.6 4.6q.275.275.275.7t-.275.7q-.275.275-.7.275t-.7-.275z"
                     />
                   </svg>
-                  <div className="absolute -bottom-40 left-0 flex h-max w-[200px] w-full flex-col gap-2 rounded-lg border border-zinc-200 bg-white p-2 shadow-lg">
+                  <div className="absolute -bottom-40 left-0 flex h-max w-[200px] flex-col gap-2 rounded-lg border border-zinc-200 bg-white p-2 shadow-lg">
                     <span className="flex flex-row items-center gap-2 rounded-lg p-2 hover:bg-zinc-100">
                       <p>Solutions 1</p>
                     </span>
