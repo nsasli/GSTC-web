81aebb3421d318b31af6e24bbd5a5887110bd9f3
Nita <47415483+nsasli@users.noreply.github.com>
2024-07-01 12:52:19 +0000

    edit Testimoni

--- src/components/CallToAction.jsx
+++ src/components/CallToAction.jsx
@@ -24,8 +24,7 @@ export function CallToAction() {
             Get started today
           </h2>
           <p className="mt-4 text-lg tracking-tight text-white">
-            It’s time to take control of your books. Buy our software so you can
-            feel like you’re doing something productive.
+          It’s time to take control of sustainable tourism. Join GSTC so you can actively contribute to positive change.
           </p>
           <Button href="/register" color="white" className="mt-10">
             Get 6 months free
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,7 +12,7 @@ export function Footer() {
           <Logo className="mx-auto h-10 w-auto" />
           <nav className="mt-10 text-sm" aria-label="quick links">
             <div className="-my-1 flex justify-center gap-x-6">
-              <NavLink href="#features">Features</NavLink>
+              <NavLink href="#features">About Us</NavLink>
               <NavLink href="#testimonials">Testimonials</NavLink>
               <NavLink href="#pricing">Pricing</NavLink>
             </div>
@@ -40,7 +40,7 @@ export function Footer() {
             </Link>
           </div>
           <p className="mt-6 text-sm text-slate-500 sm:mt-0">
-            Copyright &copy; {new Date().getFullYear()} GSTC Accreditation. All
+            Copyright &copy; {new Date().getFullYear()} Nita Septiani. All
             rights reserved.
           </p>
         </div>
