50963e6bb7b6ed48f8b8c142ea98798f17efb97b
Handi Priyono <handimacbook@gmail.com>
2024-07-01 09:35:25 +0700

    fix: wording

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -66,9 +66,10 @@ function MobileNavigation() {
         transition
         className="absolute inset-x-0 top-full mt-4 flex origin-top flex-col rounded-2xl bg-white p-4 text-lg tracking-tight text-slate-900 shadow-xl ring-1 ring-slate-900/5 data-[closed]:scale-95 data-[closed]:opacity-0 data-[enter]:duration-150 data-[leave]:duration-100 data-[enter]:ease-out data-[leave]:ease-in"
       >
-        <MobileNavLink href="#features">Features</MobileNavLink>
-        <MobileNavLink href="#testimonials">Testimonials</MobileNavLink>
-        <MobileNavLink href="#pricing">Pricing</MobileNavLink>
+        <MobileNavLink href="#features">About</MobileNavLink>
+        <MobileNavLink href="#testimonials">GSTC Criteria</MobileNavLink>
+        <MobileNavLink href="#pricing">Accreditation</MobileNavLink>
+        <MobileNavLink href="#pricing">Certification</MobileNavLink>
         <hr className="m-2 border-slate-300/40" />
         <MobileNavLink href="/login">Sign in</MobileNavLink>
       </PopoverPanel>
@@ -86,12 +87,12 @@ export function Header() {
               <Logo className="h-10 w-auto" />
             </Link>
             <div className="hidden md:flex md:gap-x-6">
-              <NavLink href="#features">Features</NavLink>
-              <NavLink href="#testimonials">Testimonials</NavLink>
-              <NavLink href="#pricing">Pricing</NavLink>
+              <NavLink href="#features">About</NavLink>
+              <NavLink href="#testimonials">GSTC Criteria</NavLink>
+              <NavLink href="#pricing">Accreditation</NavLink>
               <NavLink href="#">
                 <button class="group relative flex flex-row gap-2 overflow-hidden transition-all duration-200 focus:overflow-visible">
-                  Solutions
+                  Certification
                   <svg
                     class="rotate-90 group-focus:rotate-180"
                     xmlns="http://www.w3.org/2000/svg"
