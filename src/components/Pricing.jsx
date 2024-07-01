21a648a8c516079058c8b26678956f19035b3a65
Nita <47415483+nsasli@users.noreply.github.com>
2024-07-01 12:48:23 +0000

    edit Testimoni

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -111,8 +111,7 @@ export function Pricing() {
             for everyone.
           </h2>
           <p className="mt-4 text-lg text-slate-400">
-            It doesn’t matter what size your business is, our software won’t
-            work well for you.
+          No matter the size of your tourism destination, GSTC standards will work well for you
           </p>
         </div>
         <div className="-mx-4 mt-16 grid max-w-2xl grid-cols-1 gap-y-10 sm:mx-auto lg:-mx-8 lg:max-w-none lg:grid-cols-3 xl:mx-0 xl:gap-x-8">
